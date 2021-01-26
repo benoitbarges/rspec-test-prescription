4c6892da4699c7ef07cda114527c30af7b94cf7c
benoit barges <benoitbarges@gmail.com>
2021-01-26 12:36:10 +0100

    generate Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_112743) do
+ActiveRecord::Schema.define(version: 2021_01_26_113555) do
 
   create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
